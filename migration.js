ad5ffadc58dcf19fae141d652fde0d48380fec59
Alex <alexlawn333@gmail.com>
2021-07-14 10:48:52 +0100

    creating table called Series

--- migration.js
+++ migration.js
@@ -4,6 +4,7 @@ const db = new sqlite3.Database('./database.sqlite');
 db.serialize(() => {                           
     db.run("DROP TABLE IF EXISTS Artist");
     db.run("CREATE TABLE Artist (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, date_of_birth TEXT NOT NULL, biography TEXT NOT NULL, is_currently_employed INTEGER NOT NULL DEFAULT 1)");
+    db.run("CREATE TABLE Series (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL)");
   });
   
   
